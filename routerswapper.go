169685a65c589cb52205af91a4983fbe6c2fd9a3
Andrew Heberle <andrew.heberle@gmail.com>
2019-06-23 22:17:32 +0800

    Comments

--- routerswapper.go
+++ routerswapper.go
@@ -1,3 +1,6 @@
+// Package routerswapper implements a simple process to swap a Golang HTTP
+// router (gorilla/mux etc) during runtime. This is aimed at allowing
+// route/handler changes based on a configuration change or update.
 package routerswapper
 
 import (
@@ -16,7 +19,7 @@ type RouterSwapper struct {
 	rt router
 }
 
-// Swap replaces the current router
+// Swap replaces the current router.
 func (rs *RouterSwapper) Swap(rt router) {
 	rs.mu.Lock()
 	rs.rt = rt
@@ -24,6 +27,7 @@ func (rs *RouterSwapper) Swap(rt router) {
 }
 
 // New creates a new RouteSwapper based on the provided router
+// which can then be used where ServeHTTP would be used
 func New(rt router) *RouterSwapper {
 	rs := new(RouterSwapper)
 	rs.rt = rt
