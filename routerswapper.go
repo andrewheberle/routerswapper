96af35591654e7afea8944d1143d73676c45f4ea
Andrew Heberle <andrew.heberle@gmail.com>
2019-06-23 19:31:46 +0800

    Put pointers back

--- routerswapper.go
+++ routerswapper.go
@@ -13,18 +13,18 @@ type Router interface {
 // RouterSwapper is our only type
 type RouterSwapper struct {
 	mu sync.RWMutex
-	rt Router
+	rt *Router
 }
 
 // Swap replaces the current Router
-func (rs *RouterSwapper) Swap(rt Router) {
+func (rs *RouterSwapper) Swap(rt *Router) {
 	rs.mu.Lock()
 	rs.rt = rt
 	rs.mu.Unlock()
 }
 
 // NewRouterSwapper creates a new RouteSwapper based on the passed Router
-func NewRouterSwapper(rt Router) (rs *RouterSwapper) {
+func NewRouterSwapper(rt *Router) (rs *RouterSwapper) {
 	rs.rt = rt
 	return rs
 }
