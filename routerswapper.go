3a63fb8e4ef090f5e344a49b4b2378fb5195f015
Andrew Heberle <andrew.heberle@gmail.com>
2019-06-23 18:58:29 +0800

    rename

---
+++ routerswapper.go
@@ -0,0 +1,44 @@
+package routerswapper
+
+import (
+	"net/http"
+	"sync"
+)
+
+// Router interface is satisfied by any type that exports ServeHTTP
+type Router interface {
+	ServeHTTP(http.ResponseWriter, *http.Request)
+}
+
+// RouterHandler satisfies the Router interface
+type RouterHandler struct {
+	Router
+}
+
+// RouterSwapper is our only type
+type RouterSwapper struct {
+	mu sync.RWMutex
+	rt *RouterHandler
+}
+
+// Swap replaces the current RouterHandler
+func (rs *RouterSwapper) Swap(rt *RouterHandler) {
+	rs.mu.Lock()
+	rs.rt = rt
+	rs.mu.Unlock()
+}
+
+// NewRouteSwapper creates a new RouteSwapper based on the passer RouterHandler
+func NewRouteSwapper(rt *RouterHandler) (rs *RouterSwapper) {
+	rs.rt = rt
+	return rs
+}
+
+// ServeHTTP method for the RouterSwapper
+func (rs *RouterSwapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	rs.mu.RLock()
+	rt := rs.rt
+	rs.mu.RUnlock()
+
+	rt.ServeHTTP(w, r)
+}
